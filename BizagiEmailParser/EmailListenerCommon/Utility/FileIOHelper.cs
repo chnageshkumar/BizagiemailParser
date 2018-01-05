4db016b07e68d9a0b554bd7812d0620dc63e26f0
abhiram144 <venkata.abhiram144@gmail.com>
2018-01-05 08:02:59 +0530

    Added Takeda Bizagi COmmon

---
+++ BizagiEmailParser/EmailListenerCommon/Utility/FileIOHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailListenerCommon
+{
+    public static class FileIOHelper
+    {
+        private static StreamWriter sw;
+        public static void WriteToNewFile(string path, string fileNameWithExtension, string Content)
+        {
+            var fileCompletePath = string.Format("{0}\\{1}", path, fileNameWithExtension);
+            if (System.IO.File.Exists(fileCompletePath))
+                System.IO.File.Delete(fileCompletePath);
+            sw = new System.IO.StreamWriter(System.IO.File.Create(fileCompletePath));
+            sw.Close();
+            File.WriteAllText(fileCompletePath, Content + Environment.NewLine);
+        }
+
+        public static void AppendToFile(string path, string fileNameWithExtension, string Content)
+        {
+            var fileCompletePath = string.Format("{0}\\{1}", path, fileNameWithExtension);
+            File.AppendAllText(fileCompletePath, Content + Environment.NewLine);
+        }
+    }
+}
